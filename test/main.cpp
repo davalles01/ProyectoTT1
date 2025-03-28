a1b253ca0f74ac3ba3aa78322a83fd2273785b02
davalles01 <vallesdaniel7@gmail.com>
2025-03-28 09:43:48 +0100

    Codigo

---
+++ test/main.cpp
@@ -0,0 +1,19 @@
+#include "../include/matrix.hpp"
+#include <iostream>
+
+int main(){
+	
+	Matrix M1(3,2);
+	M1(1,1) = 5.0;
+	
+	Matrix M2(3,2);
+	M2(1,1) = 3.0;
+	
+	Matrix M3 = M1 + M2;
+	
+	cout << M1 << endl;
+	cout << M2 << endl;
+	cout << M3 << endl;
+	
+	return 0;
+}
